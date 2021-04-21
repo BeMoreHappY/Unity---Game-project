f0792b483fa85d6808eec78b063975aaeae35e4f
HuntingSHEEP <a.glowacki98@gmail.com>
2021-04-21 15:37:23 +0200

    first sound -button fx

---
+++ Assets/buttonFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonFX : MonoBehaviour
+{
+    public AudioSource myFX;
+    public AudioClip hoverFX;
+    public AudioClip clickFX;
+
+    public void hoverSound()
+    {
+        myFX.PlayOneShot(hoverFX);
+    }
+    
+    public void clickSound()
+    {
+        myFX.PlayOneShot(clickFX);
+    }
+}
