ba7ae3add762f5048fe8c6aa084ec40600dc7362
HuntingSHEEP <a.glowacki98@gmail.com>
2021-04-21 21:40:30 +0200

    skills tree >> selected button color change

---
+++ Assets/buttonManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class buttonManager : MonoBehaviour
+{
+    private Color selectedColor = new Color(0.984f, 0.933f, 0.094f, 1f);
+    private Color unselectedColor = new Color(0.105f, 0.952f, 0.952f, 0.6f);
+    
+    
+    
+    void Start()
+    {
+        GetComponent<Image>().color = unselectedColor;
+    }
+    
+    public void setSelected()
+    {
+        GetComponent<Image>().color = selectedColor;
+    }
+}
